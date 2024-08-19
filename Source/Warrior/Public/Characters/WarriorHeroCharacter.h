9825864d153f611223a54031678378ac984adf32
Ashish <ashishmaan22@gmail.com>
2024-08-19 17:33:22 +0530

    hero classes

---
+++ Source/Warrior/Public/Characters/WarriorHeroCharacter.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/WarriorBaseCharacter.h"
+#include "WarriorHeroCharacter.generated.h"
+
+UCLASS()
+class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter {
+	GENERATED_BODY()
+};
